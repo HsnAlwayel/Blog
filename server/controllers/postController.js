364b107199d037dc970b8fa6ab9a56ab494ff5bd
Hassan Alwayel <hsn.alwayel@gmail.com>
2020-11-09 22:52:12 +0300

    list and post working (Server)

---
+++ server/controllers/postController.js
@@ -0,0 +1,20 @@
+const Post = require("../models/post");
+
+exports.getPosts = async (req, res) => {
+  try {
+    const posts = await Post.find();
+
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+exports.createPost = async (req, res) => {
+  try {
+    const newPost = await Post.create(req.body);
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -9,10 +9,16 @@ app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
+//Routers
+const PostRouter = require("./routes/posts");
+app.use(PostRouter);
+
+//Port Setup
+const PORT = process.env.PORT || 8000;
+
+// Db Connection
 const CONNECTION_URL =
   "mongodb+srv://admin:@Password1@cluster0.ndxt1.mongodb.net/blog?retryWrites=true&w=majority";
-
-const PORT = process.env.PORT || 8000;
 
 mongoose
   .connect(CONNECTION_URL, {
